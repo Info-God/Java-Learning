e03f3acd7f7339f3ae768a3ce5e70af7c7d51d3f
Info-God <karanvel.2005@gmail.com>
2025-02-13 23:11:10 +0530

    day 3

--- Day 3/print_number_between_range.java
+++ Day 3/print_number_between_range.java
@@ -11,11 +11,14 @@ public class print_number_between_range {
         System.out.print("Enter range 2:");
 
         int range2 = inputField.nextInt();
-
+        int count = 0;
         for(int i = 1; range1 <= range2; i++){
-            System.out.println(range1);
+            if(range1 % 2 != 0)
+                count +=1;
+                
+            
             range1 += 1;
         }
-
+        System.out.println(count);
     }
 }
